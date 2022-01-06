1f05e9ffb48c8ef1724a15cd4f0facd2b6fb64dc
Oseias Beu <oseiasbeu@outlook.com>
2022-01-05 23:11:38 -0300

    Criando blockchain básica

---
+++ script.py
@@ -0,0 +1,19 @@
+import datetime
+import hashlib
+import json
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+    def create_block(self,proof,previous_hash):
+        block = {'index': len(self.chain) + 1,
+                'timestamp':str(datetime.datetime.now()),
+                'proof': proof,
+                'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
